5f10daa4c9592b3e6c5ced73ed4642790058553f
Maricelahuaman <maricelahuamanpareja38@gmail.com>
2022-11-09 21:28:38 -0500

    Update crear_usuario.php

--- crear_usuario.php
+++ crear_usuario.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Gentelella Alela! | </title>
-f
+    
     <!-- Bootstrap -->
     <link href="Gentella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
